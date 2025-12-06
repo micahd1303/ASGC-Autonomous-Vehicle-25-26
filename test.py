5ded93b50481e82361d170fae6d0e47f62fedeec
micahd1303 <micahd1303@gmail.com>
2025-12-06 20:50:43 +0000

    Added two test python files for practice

---
+++ test.py
@@ -0,0 +1,9 @@
+import time
+
+print("ASGC Pi Test Script Running...")
+
+count = 0
+while True:
+    print("Loop count:", count)
+    count += 1
+    time.sleep(1)
---
+++ test2.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+import sys
+print("Hello from ASGC project! Python version:", sys.version)
