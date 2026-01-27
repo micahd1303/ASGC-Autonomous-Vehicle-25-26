044c5c4d5d8f9684056b69663a3a9f96412c1688
micahd1303 <micahd1303@gmail.com>
2026-01-27 03:52:14 +0000

    video location and test on pi

--- ColorCycleFSM.py
+++ ColorCycleFSM.py
@@ -4,7 +4,7 @@ import os
 from picamera2 import Picamera2
 
 # -----------------------------------
-# TOGGLE FSM PARTS
+# TOGGLE FSM PARTS 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
 # -----------------------------------
 RUN_BALLS = False
 RUN_BUCKETS = True
