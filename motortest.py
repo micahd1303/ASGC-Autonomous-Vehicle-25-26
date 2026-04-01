7eb64778c3295e24b5d6f0fc7de2ff15cacc1e4e
micahd1303 <micahd1303@gmail.com>
2026-04-01 18:39:33 +0100

    tested the neutral, but was jittery.

--- motortest.py
+++ motortest.py
@@ -3,25 +3,25 @@ from time import sleep
 
 servo = Servo(18, min_pulse_width=0.001, max_pulse_width=0.002)
 
-LOW_SPEED = 0.04   # ~2% power
+LOW_SPEED = 0.03   # ~2% power
 NEUTRAL = 0.0      # adjust if needed later
 
 print("Neutral...")
-servo.value = NEUTRAL
+servo.value = NEUTRAL # Was super jittery, not sure why. Need to look into it more. Worked on the rev client though.
 sleep(3)
 
-print("Very slow forward...")
-servo.value = NEUTRAL + LOW_SPEED
-sleep(4)
+#print("Very slow forward...")
+#servo.value = NEUTRAL + LOW_SPEED
+#sleep(4)
 
-print("Back to neutral...")
-servo.value = NEUTRAL
-sleep(3)
+#print("Back to neutral...")
+#servo.value = NEUTRAL
+#sleep(3)
 
-print("Very slow reverse...")
-servo.value = NEUTRAL - LOW_SPEED
-sleep(4)
+#print("Very slow reverse...")
+#servo.value = NEUTRAL - LOW_SPEED
+#sleep(4)
 
-print("Back to neutral...")
-servo.value = NEUTRAL
-sleep(3)
+#print("Back to neutral...")
+#servo.value = NEUTRAL
+#sleep(3)
