425dc7f4ca36eeaf30794fa75eaff5d78650efa5
micahd1303 <micahd1303@gmail.com>
2026-04-01 23:17:27 +0100

    basically same as last one.

--- motortest.py
+++ motortest.py
@@ -4,11 +4,11 @@ from time import sleep
 servo = Servo(18, min_pulse_width=0.001, max_pulse_width=0.002)
 
 LOW_SPEED = 0.03   # ~2% power
-NEUTRAL = 0.0      # adjust if needed later
+NEUTRAL = 0.025      # adjust if needed later
 
 print("Neutral...")
 servo.value = NEUTRAL # Was super jittery, not sure why. Need to look into it more. Worked on the rev client though.
-sleep(3)
+sleep(1)
 
 #print("Very slow forward...")
 #servo.value = NEUTRAL + LOW_SPEED
